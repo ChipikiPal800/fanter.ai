2f93a30010ddba85cd66b4bcf4cad9d04e106321
abcatlmfao <161179292+ChipikiPal800@users.noreply.github.com>
2026-04-18 23:25:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
 // config.js - DON'T COMMIT THIS FILE TO GITHUB!
-// add config.js to your .gitignore!
-
-const OPENROUTER_API_KEY = "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";
+const GROQ_API_KEY = "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";
