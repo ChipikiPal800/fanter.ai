1941c764150cfc37ffe1080c4cb5b6aaf3f93f3f
abcatlmfao <161179292+ChipikiPal800@users.noreply.github.com>
2026-04-18 23:04:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js - DON'T COMMIT THIS FILE TO GITHUB!
 // add config.js to your .gitignore!
 
-const HF_TOKEN = "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";
+const OPENROUTER_API_KEY = "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";
